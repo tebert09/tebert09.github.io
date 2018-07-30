a60d05f884a85dc5f7eeb640ceae7dc75ce7b320
eber0222 <eber0222@umn.edu>
2018-07-30 13:45:02 -0500

    added some styling around brewery list; removed unnecessary HTML; cleaning up JS comments, added lat & lng to breweries array;

--- js/main.js
+++ js/main.js
@@ -1,149 +1,9 @@
 
-
-
-var breweryList = {
- fiveSix: {
-   name: "56 Brewery",
-   address: "3055 NE Columbia Ave Suite 102, Minneapolis, MN 55418",
-   type: "Brewery",
-   rating: 4
- },
- sixOneTwo: {
-   name: "612 Brew",
-   address: "945 Broadway St NE, Minneapolis, MN 55413",
-   type: "Brewery",
-   rating: 4
- },
- able: {
-   name: "Able Brewery",
-   address: "1121 Quincy St NE, Minneapolis, MN 55413",
-   type: "Brewery",
-   rating: 5
- },
- bauhaus: {
-   name: "Bauhaus Brewery",
-   address: "1315 Tyler St NE, Minneapolis, MN 55413",
-   type: "Brewery",
-   rating: 3
- },
- dangerousMan: {
-   name: "Dangerous Man",
-   address: "1300 NE 2nd St, Minneapolis, MN 55413",
-   type: "Brewery",
-   rating: 5
- },
- fairState: {
-   name: "Fair State Brewery",
-   address: "2506 Central Ave NE, Minneapolis, MN 55418",
-   type: "Brewery",
-   rating: 4
- },
- headFlyer: {
-   name: "HeadFlyer Brewery",
-   address: "861 E Hennepin Ave, Minneapolis, MN 55414",
-   type: "Brewery",
-   rating: 3
- },
- indeed: {
-   name: "Indeed Brewery",
-    address: "711 NE 15th Ave, Minneapolis, MN 55413",
-   type: "Brewery",
-   rating: 4
- },
- insight: {
-   name: "Insight Brewing",
-   address: "2821 E Hennepin Ave, Minneapolis, MN 55413",
-   type: "Brewery",
-   rating: 3
- },
- norseman: {
-   name: "Norseman Distillery",
-   address: "451 Taft St NE, Minneapolis, MN 55413",
-   type: "Distillery",
-   rating: 4
- },
- sociableCider: {
-   name: "Sociable Cider Werks",
-   address: "1500 Fillmore St NE, Minneapolis, MN 55413",
-   type: "Cider ",
-   rating: 5
- }
-}
-
-$('.restaurant').html('<div>Hello</div>');
-
-// take breweryList object, convert to HTML
-// properties go into rest-info div
-
-
-// function Brewery(name, address, type, rating) {
-//     this.name = name; 
-//     this.address = address; 
-//     this.type = type; 
-//     this.rating = rating; 
-// }
-
-// var tattersall = new Brewery('Tattersall Distilling', '1620 Central Ave NE #150, Minneapolis, MN 55413', 'Distillery', 3);
-
-// updating the dom
-// create new breweries
-// take values from input and put inside HTML
-// use mustache
-$('form').on('submit', function(e) {
-  e.preventDefault();
-
-  var userInput = {
-    name: $('#name').val(), 
-    address: $('#address').val(),
-    type: $('#type').val(), 
-    rating: $('#rating').val()
-  }
-
-  var source = $("#brewery").html();
-  var template = Handlebars.compile(source);
-  var newListItemHTML = template(userInput); 
-
-  $('.rest-info').append(newListItemHTML);
-});
-
-// how do I get these new breweries to stay on the page? 
-
-/////// Search 
-// as user searches, restaurants filter below
-// restaurants animate in a quick, scramble fashion
-
-
-/////// Favorites
-// user clicks the star 
-// star changes colors
-// restaurant is added to favorites list (side column)
-
-/////// View more
-// on page load, user sees first ~4 restaurants
-// when they click "view more" the next 4 will show up
-// page gets longer
-
-/////// Rating
-// to rate the restaurant, user clicks on the restaurant box to flip the card
-// card flips and grows with animation
-// can rate (1-5 scale) and comment
-// on submit, card flips to "thank you for your rating" screen
-// click card again, card flips back to info
-
-/////// JS Object
-// each restaurant is an object 
-
-
-
-
-
-
 //////// Google Map = API
-// on page load, google maps shows user's location
-// if location services off, map shows minneapolis
-// map should zoom, have markers of restaurants
-
-
+// on page load, google maps current list of breweries with markers
+// map should zoom
+// objects need lat, lng
+// as user adds new brewery, marker is created
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -164,15 +24,153 @@ function initMap() {
           position: {lat: 45.006075, lng: -93.249174},
           map: map,
           title: 'Tattersall'
-        });
+    });
 
 }
 
+// create breweries array
+// each object has name, address, type, rating
+// add to this array with a user submitted form.
+var breweries = [
+ {
+   name: "56 Brewery",
+   address: "3055 NE Columbia Ave Suite 102, Minneapolis, MN 55418",
+   type: "Brewery",
+   rating: 4, 
+   lat: 45.023837, 
+   lng: -93.272108
+ },
+ {
+   name: "612 Brew",
+   address: "945 Broadway St NE, Minneapolis, MN 55413",
+   type: "Brewery",
+   rating: 4, 
+   lat: 44.999261, 
+   lng: -93.246726
+ },
+ {
+   name: "Able Brewery",
+   address: "1121 Quincy St NE, Minneapolis, MN 55413",
+   type: "Brewery",
+   rating: 5, 
+   lat: 44.999666, 
+   lng: -93.251025
+ },
+ {
+   name: "Bauhaus Brewery",
+   address: "",
+   type: "Brewery",
+   rating: 3, 
+   lat: 45.001165, 
+   lng: -93.244679
+ },
+ {
+   name: "Dangerous Man",
+   address: "1300 NE 2nd St, Minneapolis, MN 55413",
+   type: "Brewery",
+   rating: 5, 
+   lat: 45.001288, 
+   lng: -93.266419
+ },
+ {
+   name: "Fair State Brewery",
+   address: "2506 Central Ave NE, Minneapolis, MN 55418",
+   type: "Brewery",
+   rating: 4, 
+   lat: 45.013777,  
+   lng: -93.247565
+ },
+ {
+   name: "HeadFlyer Brewery",
+   address: "861 E Hennepin Ave, Minneapolis, MN 55414",
+   type: "Brewery",
+   rating: 3, 
+   lat: 44.992155, 
+   lng: -93.243879
+ },
+ {
+   name: "Indeed Brewery",
+   address: "711 NE 15th Ave, Minneapolis, MN 55413",
+   type: "Brewery",
+   rating: 4, 
+   lat: 45.003504, 
+   lng: -93.251146
+ },
+ {
+   name: "Insight Brewing",
+   address: "2821 E Hennepin Ave, Minneapolis, MN 55413",
+   type: "Brewery",
+   rating: 3, 
+   lat: 44.992008, 
+   lng: -93.212638
+ },
+ {
+   name: "Sociable Cider Werks",
+   address: "1500 Fillmore St NE, Minneapolis, MN 55413",
+   type: "Cider",
+   rating: 5, 
+   lat: 45.004698, 
+   lng: -93.243990
+ }
+];
+
+// take breweryList object, convert to HTML
+// properties go into rest-info div
+// for every object in the breweries array, loop through, and display name, address, type, and rating
+// add divs around each property
+$(function() {
+  for (var i = 0; i < breweries.length; i++) {
+    var txt1 = '<div class="name">' + breweries[i].name + '</div>';
+    var txt2 = '<div class="address">' + breweries[i].address + '</div>';
+    var txt3= '<div class="type">' + breweries[i].type + '</div>';
+    var txt4= '<div class="rating">' + breweries[i].rating + '</div>';
+    $('.restaurants').append('<div class="rest-info">' + txt1 + txt2 + txt3 + txt4 + '</div>');
+  }
+});
+
+
+
+// create new breweries from user submitted form
+// take values from input and put inside rest-info div
+// use handlebars to template
+$('form').on('submit', function(e) {
+  e.preventDefault();
+
+  var userInput = {
+    name: $('#name').val(), 
+    address: $('#address').val(),
+    type: $('#type').val(), 
+    rating: $('#rating').val()
+  }
+
+  var source = $("#brewery").html();
+  var template = Handlebars.compile(source);
+  var newListItemHTML = template(userInput); 
+
+  $('.rest-info').append(newListItemHTML);
+});
+
+
+
+
+
+
+
+
+
 
 /////// Questions
-// What other JS requirements? (object, DOM manipulation, API/Firebase, GIT)
-// think about data structure in firebase (users, ratings, )
-// keep in mind what needs to be grouped together.
+// how do I get these new breweries to stay on the page? 
+// why can't I put HTML inside the handlebars template?
+// how to create new map markers from objects and user submissions
 
 
 
+////// Need to have
+// variables - done
+// object - done
+// API (google) - done
+// updating DOM (new brewery) - done
+// git - done
+// Firebase 
+// CRUD
